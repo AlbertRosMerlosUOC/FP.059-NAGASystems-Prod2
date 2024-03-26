b836f6808a34679ad2195015bb9c50d9ddbb6925
Albert Ros <arosme@uoc.edu>
2024-03-26 21:16:31 +0100

    HotelSol en EntityFramework

---
+++ Controllers/HabitacionController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CapaModelo;
+using FP._059_NAGASystems_Prod2.Data;
+
+namespace FP._059_NAGASystems_Prod2.Controllers
+{
+    public class HabitacionController : Controller
+    {
+        private readonly FP_059_NAGASystems_Prod2Context _context;
+
+        public HabitacionController(FP_059_NAGASystems_Prod2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Habitacion
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Habitacion.ToListAsync());
+        }
+
+        // GET: Habitacion/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var habitacion = await _context.Habitacion
+                .FirstOrDefaultAsync(m => m.Numero == id);
+            if (habitacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(habitacion);
+        }
+
+        // GET: Habitacion/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Habitacion/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Numero,TipoHabitacion,Estado")] Habitacion habitacion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(habitacion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(habitacion);
+        }
+
+        // GET: Habitacion/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var habitacion = await _context.Habitacion.FindAsync(id);
+            if (habitacion == null)
+            {
+                return NotFound();
+            }
+            return View(habitacion);
+        }
+
+        // POST: Habitacion/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Numero,TipoHabitacion,Estado")] Habitacion habitacion)
+        {
+            if (id != habitacion.Numero)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(habitacion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HabitacionExists(habitacion.Numero))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(habitacion);
+        }
+
+        // GET: Habitacion/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var habitacion = await _context.Habitacion
+                .FirstOrDefaultAsync(m => m.Numero == id);
+            if (habitacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(habitacion);
+        }
+
+        // POST: Habitacion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var habitacion = await _context.Habitacion.FindAsync(id);
+            if (habitacion != null)
+            {
+                _context.Habitacion.Remove(habitacion);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HabitacionExists(int id)
+        {
+            return _context.Habitacion.Any(e => e.Numero == id);
+        }
+    }
+}
